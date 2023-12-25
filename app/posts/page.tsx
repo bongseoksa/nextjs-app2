6128580c6fa04fa1bcdc26dd6880858ff3d537e1
bongseoksa <qhdtjr4517@gmail.com>
2023-12-25 17:10:27 +0900

    [Commit 내용] Post 항목 추가 * Post 리스트 조회실패시 자동 에러 컴포넌트 발생 * 새로운 Post 항목 추가

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -9,6 +9,12 @@ async function getPost() {
         cache: 'no-store',
     });
 
+    // 에러 발생시 자동으로 에러 페이지로 이동
+    if(!res.ok) {
+        // 현재 파일 기준에서 가장 가까이 있는 error.js 파일 activated
+        throw new Error(`failed to fetch data`);
+    }
+
     const data = await res.json();
     return data?.items as any[];
 }
